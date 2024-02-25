3be0f731ea3312c958b7e7c9bccc57f42efe46f4
shogunIncognito <saidsamuel2006@hotmail.com>
2024-02-25 11:44:57 -0500

    first commit

---
+++ src/helpers/data.js
@@ -0,0 +1,11 @@
+export const routes = [
+    { label: 'Inicio', url: '/' },
+    { label: 'Empresa', url: '/empresa' },
+    { label: 'Mantenimiento', url: '/mantenimiento' },
+    { label: 'Calidad', url: '/calidad' },
+    { label: 'Sistemas solares', url: '/sistemasolares' },
+    { label: 'Redes', url: '/redes' },
+    { label: 'Seguridad', url: '/seguridad' },
+    { label: 'Diseño web', url: '/disenoweb' },
+    { label: 'Licenciamiento', url: '/licenciamiento' }
+]
